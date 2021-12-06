a34eadde6a39a9d28368965268b2ea64e446a509
Daniel Kim <daniel.d.kim13@gmail.com>
2021-12-06 16:40:11 -0500

    initial window.prompt testing line

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,17 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  // prompt if an user wants to choose the # of characters for the password
+  window.prompt("How many characters do you want your password to be? Please choose between 8 and 128");
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
