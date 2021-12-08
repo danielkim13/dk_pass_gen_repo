c23be8d6405d08ab68b95fe997e5a1e91d21fc2c
Daniel Kim <daniel.d.kim13@gmail.com>
2021-12-08 18:30:58 -0500

    password criteria into function, modified variables and functions to display on the textarea on the webpage

--- assets/script.js
+++ assets/script.js
@@ -5,9 +5,6 @@ const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "
 const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const speChar = ["?", "<", ">", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", "(", ")", "_", "+", "/", ")", ":", "."];
-// global variable for user's choice .. use let variable since it will change based on user's input
-let userChoice;
-
 // global variable for password criteria... i tried to to make it a local variable but didn't work.. hmm i wonder why. (??)
 let passCrit;
 // empty array to place the result of random password
@@ -15,51 +12,14 @@ const securePassword = [];
 
 // 2. write generatePassword function... gonna write the function expression.
 const generatePassword = function () {
+  // moved this variable from global to local since this var only impacts within this function
+  let userChoice;
   // 2.1. prompt - length of password
   userChoice = prompt("How long do your want your password to be? Please choose between 8 and 128");
   // 2.1.1. user need to write value between 8-128 if not continue to call the function so user can get it correctly
   if (userChoice >= 8 && userChoice <= 128) {
-    // 2.2. prompt - criteria for password (upper/lower/number/special character) when met the criteria of password length being btwn 8-128, prompt password criteria
-    const confirmUpper = confirm("Would you like to have some upper case in your password?");
-    const confirmLower = confirm("Would you like to have some lower case in your password?");
-    const confirmNum = confirm("Would you like to have some numbers in your password?");
-    const confirmSpec = confirm("Would you like to have some special characters in your password?");
-
-    if (confirmUpper && confirmLower && confirmNum && confirmSpec) {
-      passCrit = upperCase.concat(lowerCase, num, speChar);
-    } else if (!confirmUpper && confirmLower && confirmNum && confirmSpec) {
-      passCrit = lowerCase.concat(num, speChar);
-    } else if (confirmUpper && !confirmLower && confirmNum && confirmSpec) {
-      passCrit = upperCase.concat(num, speChar);
-    } else if (confirmUpper && confirmLower && !confirmNum && confirmSpec) {
-      passCrit = upperCase.concat(lowerCase, speChar);
-    } else if (confirmUpper && confirmLower && confirmNum && !confirmSpec) {
-      passCrit = upperCase.concat(lowerCase, num);
-    } else if (!confirmUpper && !confirmLower && confirmNum && confirmSpec) {
-      passCrit = num.concat(speChar);
-    } else if (!confirmUpper && confirmLower && !confirmNum && confirmSpec) {
-      passCrit = lowerCase.concat(speChar);
-    } else if (!confirmUpper && confirmLower && confirmNum && !confirmSpec) {
-      passCrit = lowerCase.concat(num);
-    } else if (confirmUpper && !confirmLower && !confirmNum && confirmSpec) {
-      passCrit = upperCase.concat(speChar);
-    } else if (confirmUpper && !confirmLower && confirmNum && !confirmSpec) {
-      passCrit = upperCase.concat(num);
-    } else if (confirmUpper && confirmLower && !confirmNum && !confirmSpec) {
-      passCrit = upperCase.concat(lowerCase);
-    } else if (!confirmUpper && !confirmLower && !confirmNum && confirmSpec) {
-      passCrit = speChar;
-    } else if (!confirmUpper && !confirmLower && confirmNum && !confirmSpec) {
-      passCrit = num;
-    } else if (!confirmUpper && confirmLower && !confirmNum && !confirmSpec) {
-      passCrit = lowerCase;
-    } else if (confirmUpper && !confirmLower && !confirmNum && !confirmSpec) {
-      passCrit = upperCase;
-    } else if (!confirmUpper && !confirmLower && !confirmNum && !confirmSpec) {
-      alert("You need to choose at least one password criteria. Try again!");
-      // how can I kick it back to the password criteria prompt (??)
-      return;
-    }
+    // 2.2. prompt - criteria for password ==> decided to create it as a function expression.
+    passCriteria();
   } else if (userChoice === null) {
     // if the user click 'cancel' button then just exit out
     return;
@@ -78,7 +38,7 @@ const generatePassword = function () {
     const randomPass = passCrit[Math.floor(Math.random() * passCrit.length)];
     securePassword.push(randomPass);
   }
-  securePassword.join("");
+  // console.log(securePassword.join(""));
 };
 
 // Get references to the #generate element
@@ -86,13 +46,56 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = securePassword.join("");
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 // Question 1. how can I display the random password value to the webpage
+const passCriteria = function () {
+  const confirmUpper = confirm("Would you like to have some upper case in your password?");
+  const confirmLower = confirm("Would you like to have some lower case in your password?");
+  const confirmNum = confirm("Would you like to have some numbers in your password?");
+  const confirmSpec = confirm("Would you like to have some special characters in your password?");
+
+  // 16 available cases.. list them below.
+  if (confirmUpper && confirmLower && confirmNum && confirmSpec) {
+    passCrit = upperCase.concat(lowerCase, num, speChar);
+  } else if (!confirmUpper && confirmLower && confirmNum && confirmSpec) {
+    passCrit = lowerCase.concat(num, speChar);
+  } else if (confirmUpper && !confirmLower && confirmNum && confirmSpec) {
+    passCrit = upperCase.concat(num, speChar);
+  } else if (confirmUpper && confirmLower && !confirmNum && confirmSpec) {
+    passCrit = upperCase.concat(lowerCase, speChar);
+  } else if (confirmUpper && confirmLower && confirmNum && !confirmSpec) {
+    passCrit = upperCase.concat(lowerCase, num);
+  } else if (!confirmUpper && !confirmLower && confirmNum && confirmSpec) {
+    passCrit = num.concat(speChar);
+  } else if (!confirmUpper && confirmLower && !confirmNum && confirmSpec) {
+    passCrit = lowerCase.concat(speChar);
+  } else if (!confirmUpper && confirmLower && confirmNum && !confirmSpec) {
+    passCrit = lowerCase.concat(num);
+  } else if (confirmUpper && !confirmLower && !confirmNum && confirmSpec) {
+    passCrit = upperCase.concat(speChar);
+  } else if (confirmUpper && !confirmLower && confirmNum && !confirmSpec) {
+    passCrit = upperCase.concat(num);
+  } else if (confirmUpper && confirmLower && !confirmNum && !confirmSpec) {
+    passCrit = upperCase.concat(lowerCase);
+  } else if (!confirmUpper && !confirmLower && !confirmNum && confirmSpec) {
+    passCrit = speChar;
+  } else if (!confirmUpper && !confirmLower && confirmNum && !confirmSpec) {
+    passCrit = num;
+  } else if (!confirmUpper && confirmLower && !confirmNum && !confirmSpec) {
+    passCrit = lowerCase;
+  } else if (confirmUpper && !confirmLower && !confirmNum && !confirmSpec) {
+    passCrit = upperCase;
+  } else if (!confirmUpper && !confirmLower && !confirmNum && !confirmSpec) {
+    alert("You need to choose at least one password criteria. Try again!");
+    // how can I kick it back to the password criteria prompt (??)
+    passCriteria();
+  }
+};
