ae412a1806c431973c0194a44b04f13bf9fecf40
Daniel Kim <daniel.d.kim13@gmail.com>
2021-12-07 22:28:11 -0500

    established the global variables for the password

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 // Pseudocode
-// 1. establish global variables of all the possible password character types (capital letter, lower case, number and special character)
+
 // 2. write generatePassword function
 // 2.1. prompt - length of password
 // 2.2. prompt - criteria for password (cap, lower case, number, special character)
@@ -7,6 +7,11 @@
 // 3. for loop function for the randomize the password
 // 4. connect everything where the password shows on the page or perhaps alert.
 
+// 1. establish global variables of all the possible password character types (capital letter, lower case, number and special character)
+const upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const speChar = ["?", "<", ">", "~", "!", "@", "#", "$", "%", "^", "&", "*", "(", "(", ")", "_", "+", "/", ")", ":", "."];
 // Assignment code here
 
 // Get references to the #generate element
