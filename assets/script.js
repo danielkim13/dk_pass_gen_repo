7eaaa2c7241141035f02e8bb97912ce6052590bb
Daniel Kim <daniel.d.kim13@gmail.com>
2021-12-07 22:08:10 -0500

    pseudocode written for the password generator

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,12 @@
+// Pseudocode
+// 1. establish global variables of all the possible password character types (capital letter, lower case, number and special character)
+// 2. write generatePassword function
+// 2.1. prompt - length of password
+// 2.2. prompt - criteria for password (cap, lower case, number, special character)
+// 2.3. based on the selection, result happens and that goes into the for loop
+// 3. for loop function for the randomize the password
+// 4. connect everything where the password shows on the page or perhaps alert.
+
 // Assignment code here
 
 // Get references to the #generate element
@@ -5,9 +14,6 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  // prompt if an user wants to choose the # of characters for the password
-  window.prompt("How many characters do you want your password to be? Please choose between 8 and 128");
-
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
